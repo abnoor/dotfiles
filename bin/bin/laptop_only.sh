fceec697da12c44626e69a93c5b9699a360486cd
Abdi Noor <abnoor@gmail.com>
2019-11-24 15:57:15 +0000

    changed for yoga s1

--- bin/bin/laptop_only.sh
+++ bin/bin/laptop_only.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output VGA-1 --off
+xrandr --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off
